a792cbc4f8320c01add8866292df4746972a5fad
Ankit Raj Mahapatra <54436840+Ankit404butfound@users.noreply.github.com>
2021-03-17 19:03:45 +0530

    Delete hardcor directory

--- hardcor/home/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from home.models import joinus
-# Register your models here.
-admin.site.register(joinus)
--- hardcor/home/migrations/__init__.py
+++
@@ -1,4 +0,0 @@
-#code written by mayvid
-
-#Telegram username - @mayvidxd
-#Instagram Username - @mayvidxd
